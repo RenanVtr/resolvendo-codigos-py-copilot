1669de70d67a0aeec66442fbfa06a44d943bd7e4
Renan Garcia <renanvitorg@hotmail.com>
2024-09-18 19:33:08 +0000

    Resolucao do primeiro desafio de codigo

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,8 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+nome = input("Digite seu primeiro nome:")
+sobrenome = input("Digite seu sobrenome:")
+
+nomeCompleto = nome + " " + sobrenome
+
+print(f"Seu nome completo é: {nomeCompleto}")
