5182238f8411047ae1ad255818378183c08e99c8
Renan Garcia <renanvitorg@hotmail.com>
2024-09-18 20:13:32 +0000

    Adicionando resolucao do terceiro desafio

---
+++ num_pares_impares.py
@@ -0,0 +1,9 @@
+# Como entrada, receba um número inteiro e verifique se ele é par ou ímpar.
+
+print("Vamos verificar se o numero é par ou impar?")
+num = int(input("Digite o numero: "))
+
+if (num % 2 == 0)
+    print(f"Numero {num} é par")
+else
+    print(f"Numero {num} é impar")
--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,18 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+num1 = float(input("Digite primeiro numero: "))
+operacao = input("Digite o tipo de operacao desejada (+,-,/,*): ")
+num2 = float(input("Digite segundo numero: "))
+
+match operacao:
+
+    case "+":
+        print( num1 + num2)
+    case "-":
+        print(num1 - num2)
+    case "/":
+        print(num1 / num2)
+    case "*":
+        print(num1 * num2)
+    case _:
+        print("Operador invaido")
